bcf5d0a3a32621fab09e014e845838f463393f14
Silk Manuel <silk552001@gmail.com>
2024-11-11 08:36:20 +0100

    Spring initialize

---
+++ springsecurity/src/test/java/silk/example/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package silk.example.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
